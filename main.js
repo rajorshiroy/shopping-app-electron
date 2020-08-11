0941981cefe089578e21babf30ab42ddbadcb019
Rajorshi Roy <rajorshi.me@gmail.com>
2020-08-12 04:38:46 +0530

    add items functionality added

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-const electron = require('electron');
+const {app, BrowserWindow, Menu, ipcMain} = require('electron');
 const url = require('url');
 const path = require('path');
 
-
-const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
 let addWindow;
@@ -11,7 +9,11 @@ let addWindow;
 // listen for app to be ready
 app.on('ready', () => {
     // create new window
-    mainWindow = new BrowserWindow();
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
 
     // load html file into the window
     mainWindow.loadURL(url.format({
@@ -19,6 +21,9 @@ app.on('ready', () => {
         protocol: 'file:',
         slashes: true
     }));
+
+    // handle garbage collection
+    // addWindow.on('close', () => addWindow = null);
 
     // close all windows when app is closed
     mainWindow.on('closed', () => app.quit())
@@ -51,10 +56,19 @@ const mainMenuTemplate = [
 
 ]
 
+// catch item:add
+ipcMain.on('item:add', (event, item) => {
+    console.log(item);
+    mainWindow.webContents.send('item:add', item);
+    addWindow.close();
+});
 
 const createAddWindow = () => {
 // create add window
     addWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
         width: 300,
         height: 200,
         title: 'Shopping List Item'
@@ -67,3 +81,25 @@ const createAddWindow = () => {
         slashes: true
     }));
 }
+
+// push an empty menu object on mac
+if (process.platform === 'darwin')
+    mainMenuTemplate.unshift({});
+
+
+// add developer tools menu on dev build
+if (process.env.NODE_ENV !== 'production')
+    mainMenuTemplate.push({
+            label: 'Developer Toold',
+            submenu: [
+                {
+                    label: 'Toggle Dev Tools',
+                    accelerator: process.platform === 'darwin' ? 'Command+I' : 'Ctrl+I',
+                    click(item, focusedWindow) {
+                        focusedWindow.toggleDevTools();
+                    }
+                },
+                {role: 'reload'}
+            ]
+        }
+    )
